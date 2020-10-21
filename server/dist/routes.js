47a7f8e13f5be2a70d4a814a64cb9dcae10172f8
Emanoel Trevisol <emanoel.trevisol@gmail.com>
2020-10-21 04:36:48 -0300

    build

--- server/dist/routes.js
+++ server/dist/routes.js
@@ -7,23 +7,23 @@ var express_1 = __importDefault(require("express"));
 var boom_1 = __importDefault(require("@hapi/boom"));
 var ProductApi_1 = __importDefault(require("./products/ProductApi"));
 var path_1 = __importDefault(require("path"));
-var serverPublicPath = path_1.default.resolve(__dirname, '/public/');
+var serverPublicPath = path_1.default.resolve(__dirname, '/public');
 function default_1(app) {
     app.use('/api/products', ProductApi_1.default);
-    app.use('/css', express_1.default.static(serverPublicPath + "css"));
-    app.use('/fonts', express_1.default.static(serverPublicPath + "fonts"));
-    app.use('/img', express_1.default.static(serverPublicPath + "img"));
-    app.use('/js', express_1.default.static(serverPublicPath + "js"));
-    app.use('/assets', express_1.default.static(serverPublicPath + "assets"));
+    app.use('/css', express_1.default.static(serverPublicPath + "/css"));
+    app.use('/fonts', express_1.default.static(serverPublicPath + "/fonts"));
+    app.use('/img', express_1.default.static(serverPublicPath + "/img"));
+    app.use('/js', express_1.default.static(serverPublicPath + "/js"));
+    app.use('/assets', express_1.default.static(serverPublicPath + "/assets"));
     app.all('/*', function (req, res) {
         res
             .status(200)
             .set({
             'content-type': 'text/html; charset=utf-8',
         })
-            .sendFile(serverPublicPath + "index.html");
+            .sendFile(serverPublicPath + "/index.html");
     });
-    app.use(express_1.default.static(serverPublicPath + "static"));
+    app.use(express_1.default.static(serverPublicPath + "/static"));
     // Error handling
     app.use(function (err, req, res, _) {
         var error = boom_1.default.isBoom(err) ? err : boom_1.default.boomify(err);
