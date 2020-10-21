c31ca77d43736494fd91c3d14076c653798ad34a
Emanoel Trevisol <emanoel.trevisol@gmail.com>
2020-10-21 04:31:07 -0300

    build front to server/dist

--- server/dist/validation/validationOptions.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.listDefaultLimitOption = exports.listLimitOptions = void 0;
-// const options = {
-var listLimitOptions = [10, 25, 50, 100];
-exports.listLimitOptions = listLimitOptions;
-var listDefaultLimitOption = 10;
-exports.listDefaultLimitOption = listDefaultLimitOption;
-//# sourceMappingURL=validationOptions.js.map
--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  outputDir: '../server/public',
+  outputDir: '../server/dist',
   lintOnSave: true,
   runtimeCompiler: true,
   devServer: {
